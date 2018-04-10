df865317d4b92f819b0a2f5284ce64511bd387f7
Alex Kremiansky <alextesy@gmail.com>
2018-04-10 18:42:45 +0300

    login

--- javascript.js
+++ javascript.js
@@ -1,3 +1,30 @@
+var userList=['alex'];
+var passList=['12345'];
+
 $(document).ready(function() {
-    $('body').not("#wellcome").hide();
-    });
+    $("#wrapper").children().hide();
+    $("#wellcome").show();
+    
+});
+
+function login(){
+    $("#wrapper").children().hide();
+    $("#login").show();
+}
+
+function checkUser(){
+    var nameValue = document.getElementById("userName").value;
+    var passValue=document.getElementById("passWord").value;
+    for(var i=0;i<userList.length;i++){
+        if(userList[i]==nameValue){
+            if(passList[i]==passValue){
+                $("#wrapper").children().hide();
+                Start()
+                return;
+            }
+            else
+                break;
+        }
+    }
+    alert("Username not right")
+}
